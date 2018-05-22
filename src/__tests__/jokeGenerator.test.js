d58422f6f4283d61d9998d1404e701b00c138627
Arun Woosaree <awoosaree@atb.com>
2018-05-22 11:02:27 -0600

    ok

--- src/__tests__/jokeGenerator.test.js
+++ src/__tests__/jokeGenerator.test.js
@@ -1,10 +1,15 @@
-import { render } from "react-testing-library";
+import { render, Simulate } from "react-testing-library";
 import "dom-testing-library/extend-expect";
 import React from "react";
 import Joke from "../joke";
-
+import JokeGenerator from "../jokeGenerator";
+//first test
 test("Joke component recieves props and then renders text", () => {
   const { getByTestId } = render(<Joke text="The funniest joke" />);
-
   expect(getByTestId("joke-text")).toHaveTextContent("The funniest joke");
+});
+
+test("JokeGenerator component fetches a random joke and renders it", () => {
+  const { getByText } = render(<JokeGenerator />);
+  expect(getByText("You haven't loaded any joke yet!")).toBeInTheDOM();
 });
--- src/jokeGenerator.js
+++ src/jokeGenerator.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 
 export default class JokeGenerator extends Component {
-  state = { joke: null };
+  state = {
+    joke: null
+    // loading: false
+  };
+
+  loadJoke = async () => {
+    this.setState({ loading: true });
+  };
 
   render() {
     const { joke } = this.state;
     return (
       <React.Fragment>
-        (!joke && <div>You haven't loaded a joke yet!</div>)
+        {/* {!joke && !loading && <div>You haven't loaded a joke yet!</div>}
+        {loading && <div>Loading...</div>}
+
+        <button onClick={this.loadJoke} type="button">
+          Load a random joke
+        </button> */}
+        {!joke && <div>You haven't loaded any joke yet!</div>}
       </React.Fragment>
     );
   }
