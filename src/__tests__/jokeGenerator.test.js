974ca3cb48678ed4e300c1e219a1fd2434aa6ffa
Arun Woosaree <awoosaree@atb.com>
2018-05-22 11:47:28 -0600

    Chuck Norris doesn't needs try-catch, exceptions are too afraid to raise.

--- src/__tests__/jokeGenerator.test.js
+++ src/__tests__/jokeGenerator.test.js
@@ -1,4 +1,4 @@
-import { render, Simulate } from "react-testing-library";
+import { render, Simulate, wait } from "react-testing-library";
 import "dom-testing-library/extend-expect";
 import React from "react";
 import Joke from "../joke";
@@ -7,17 +7,29 @@ import JokeGenerator from "../jokeGenerator";
 import * as axios from "axios";
 import MockAxios from "axios-mock-adapter";
 
+const mock = new MockAxios(axios, { delayResponse: Math.random() * 500 });
+afterAll(() => mock.restore());
+
 //first test
 test("Joke component recieves props and then renders text", () => {
   const { getByTestId } = render(<Joke text="The funniest joke" />);
   expect(getByTestId("joke-text")).toHaveTextContent("The funniest joke");
 });
 
-test("JokeGenerator component fetches a random joke and renders it", () => {
-  const { getByText, queryByText } = render(<JokeGenerator />);
+//second test
+
+test("JokeGenerator component fetches a random joke and renders it", async () => {
+  mock.onGet().replyOnce(200, {
+    value: {
+      joke: "Really funny joke"
+    }
+  });
+  const { getByText, queryByText, queryByTestId } = render(<JokeGenerator />);
   expect(getByText("You haven't loaded any joke yet!")).toBeInTheDOM();
 
   Simulate.click(getByText("Load a random joke"));
   expect(queryByText("You haven't loaded any joke yet")).not.toBeInTheDOM();
   expect(queryByText("Loading...")).toBeInTheDOM();
+  await wait(() => expect(queryByText("Loading...")).not.toBeInTheDOM());
+  expect(queryByTestId("joke-text")).toBeInTheDOM();
 });
--- src/jokeGenerator.js
+++ src/jokeGenerator.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Joke from "./joke";
+const RANDOM_JOKE_URL = "https://api.icndb.com/jokes/random";
 
 export default class JokeGenerator extends Component {
   state = {
@@ -8,6 +11,9 @@ export default class JokeGenerator extends Component {
 
   loadJoke = async () => {
     this.setState({ loading: true });
+
+    const { data: { value: { joke } } } = await axios.get(RANDOM_JOKE_URL);
+    this.setState({ loading: false, joke });
   };
 
   render() {
@@ -16,6 +22,7 @@ export default class JokeGenerator extends Component {
       <React.Fragment>
         {!joke && !loading && <div>You haven't loaded any joke yet!</div>}
         {loading && <div>Loading...</div>}
+        {joke && !loading && <Joke text={joke} />}
 
         <button onClick={this.loadJoke} type="button">
           Load a random joke
